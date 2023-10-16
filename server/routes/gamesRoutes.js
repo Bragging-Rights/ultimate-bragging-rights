0fcc64ce158bc95e61af6d14e6ebb975571d2f5f
Muhammad Umar <muhammadumar03124701209@gmail.com>
2023-10-17 00:49:32 +0500

    pushed changes

--- server/routes/gamesRoutes.js
+++ server/routes/gamesRoutes.js
@@ -1,18 +1,27 @@
-const express = require('express');
-const { getLeaguesController, getSeasonsController, getGames, updateActualScoresAndCalculatePredictions, addPrediction, lockPrediction, getLeagueGames, getGamesPlayed, getResults } = require('../controllers/gamesController');
+const express = require("express");
+const {
+  getLeaguesController,
+  getSeasonsController,
+  getGames,
+  updateActualScoresAndCalculatePredictions,
+  addPrediction,
+  lockPrediction,
+  getLeagueGames,
+  getGamesPlayed,
+  getResults,
+} = require("../controllers/gamesController");
 const router = express.Router();
 
 router.get("/", getGames);
 router.post("/league-games", getLeagueGames);
 router.get("/leagues", getLeaguesController);
 router.get("/seasons", getSeasonsController);
-router.post("/lock-prediction/:id", lockPrediction);
+router.post("/lock-prediction/:id", lockPrediction); //user_id & date
 
 router.get("/games-played", getGamesPlayed);
 router.post("/add-prediction", addPrediction);
 router.put("/update-actual-scores", updateActualScoresAndCalculatePredictions);
 
 router.get("/results", getResults);
-
 
 module.exports = router;
