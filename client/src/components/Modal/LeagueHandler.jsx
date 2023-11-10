09da4fd417904504bdf83f0b5fc3a4ef933c0faa
hamd ullah <hamdullahdir98@gmail.com>
2023-11-11 01:55:48 +0500

    reister section changes

--- client/src/components/Modal/LeagueHandler.jsx
+++ client/src/components/Modal/LeagueHandler.jsx
@@ -7,58 +7,60 @@ const LeagueHandler = ({
   options,
   handleRemoveLeague,
   handleLeagueChange,
-  userLeagues,
-  addAnotherLeague,
   availableTeams,
   info,
   index,
 }) => {
+  console.log("availableTeams", availableTeams);
   return (
-    <>
-      <div className="modal-bottom relative mb-2" key={index}>
-        <button
-          onClick={() => handleRemoveLeague(index)}
-          className="bg-red-500 text-white w-7 h-7 rounded-full flex items-center justify-center transition-colors hover:bg-red-600
-               absolute right-0
-              "
-        >
-          <ImCancelCircle />
-        </button>
-        <ModalSelect
-          label="Select League"
-          options={options}
-          name={`league`}
-          value={info?.selectedLeague}
-          onChange={(e) => handleLeagueChange(e, index)}
-        />
-        <ModalInput
-          label="Create Unique Username"
-          placeholder="User Name"
-          type="text"
-          name={`username`}
-          value={info?.username}
-          onChange={(e) => handleLeagueChange(e, index)}
-        />
-        <ModalSelect
-          label="Select Your Favorite Team"
-          name={`team`}
-          value={info?.team}
-          options={[
+    <div className="modal-bottom relative mb-2" key={index}>
+      <button
+        onClick={() => handleRemoveLeague(index)}
+        className="bg-red-500 text-white w-7 h-7 rounded-full flex items-center justify-center transition-colors hover:bg-red-600 absolute right-0"
+        aria-label="Remove League"
+      >
+        <ImCancelCircle />
+      </button>
+      <ModalSelect
+        label="Select League"
+        options={options}
+        name={`league`}
+        value={info?.league}
+        onChange={(e) => {
+          handleLeagueChange(e, index);
+        }}
+      />
+      <ModalInput
+        label="Create Unique Username"
+        placeholder="Username"
+        type="text"
+        name={`username`}
+        value={info?.username}
+        onChange={(e) => handleLeagueChange(e, index)}
+        aria-label="Create Unique Username"
+      />
+      <ModalSelect
+        label="Select Your Favorite Team"
+        name={`team`}
+        value={info?.team}
+        options={[
+          {
+            label: "Select Team",
+            value: "",
+          },
+          ...(availableTeams[info.league]?.map((team) => ({
+            label: team?.displayName,
+            value: team?.fullName,
+          })) || [
             {
-              label: "Select Team",
+              label: "No team available",
               value: "",
             },
-            ...availableTeams.map((team) => {
-              return {
-                label: team?.displayName,
-                value: team?.fullName,
-              };
-            }),
-          ]}
-          onChange={(e) => handleLeagueChange(e, index)}
-        />
-      </div>
-    </>
+          ]),
+        ]}
+        onChange={(e) => handleLeagueChange(e, index)}
+      />
+    </div>
   );
 };
 
