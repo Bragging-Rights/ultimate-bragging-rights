f76bc3c54fd3555a880444968ea7abf4f22b27a2
Muhammad Umar <muhammadumar03124701209@gmail.com>
2023-12-11 22:09:05 +0500

    new logic

--- server/calculations/gameEndingPoints.js
+++ server/calculations/gameEndingPoints.js
@@ -17,14 +17,16 @@ exports.pickOvertime = (sport) => {
   //3-in reg season (hockey only) after ot, they have a shootout and the shootout is same as extra innings and the shootout
   //points are the same
   //4-in pre,reg and playoff basketball and baseball rules are always the same and there's always a dropdown to choose from
+
+  //same as extra innings for picking overtime e.g 1ot, 2ot, 3ot
 };
 
 exports.pickExtraInnings = (sport) => {
   //1-if they pick ending correctly and they also pick winner correctly they 5 points and
-  //if they pick number of exter innings correctly they get 1 (pointValue) point for each inning
+  //if they pick number of exter innings correctly they get 1 (pointValue) point for each inning (points double for each inning)
 
   //2-if they pick ending correctly and they did not pick winner correctly they 2.5 points and
-  //if they pick number of exter innings correctly they get 0.5 (pointValue) point for each inning
+  //if they pick number of exter innings correctly they get 0.5 (pointValue) point for each inning (points double for each inning )
 
   userpick === gameEnd;
   userInnings === extraInnings; //assign all points
@@ -48,6 +50,57 @@ exports.inningsCalculator = (predictedNumOfInnings, pointValue) => {
   for (let i = 0; i < predictedNumOfInnings; i++) {
     if (predictedNumOfInnings == 1) {
       return;
+    }
+    point = point + point;
+  }
+  return point;
+};
+/////////////new code//////////////////////
+
+exports.pickRegulation = (userpick, gameEnd) => {
+  if (userpick === gameEnd) {
+    return userpick ? 2 : 1; // if user also picked the winner correctly, they get 2 points, otherwise 1 point
+  } else {
+    return 0; // if user picked the ending incorrectly, they get 0 points
+  }
+};
+
+exports.pickOvertime = (userpick, gameEnd) => {
+  if (userpick === gameEnd) {
+    return userpick ? 5 : 2.5; // if user also picked the winner correctly, they get 5 points, otherwise 2.5 points
+  } else {
+    return 0; // if user picked the ending incorrectly, they get 0 points
+  }
+};
+
+exports.pickExtraInnings = (userpick, gameEnd, userInnings, extraInnings) => {
+  let points = 0;
+  if (userpick === gameEnd) {
+    points += userpick ? 5 : 2.5; // if user also picked the winner correctly, they get 5 points, otherwise 2.5 points
+  }
+  if (userInnings === extraInnings) {
+    // points += userpick ? 1 : 0.5; // if user picked the number of extra innings correctly, they get additional points
+    // this one need to be fixed
+  }
+  return points;
+};
+
+exports.pickShootout = (userpick, gameEnd, userInnings, extraInnings) => {
+  let points = 0;
+  if (userpick === gameEnd) {
+    points += userpick ? 7 : 3.5; // if user also picked the winner correctly, they get 5 points, otherwise 2.5 points
+  }
+  // if (userInnings === extraInnings) {
+  //   points += userpick ? 0.2 : 0.1; // if user picked the number of extra innings correctly, they get additional points
+  // }
+  return points;
+};
+
+exports.inningsCalculator = (predictedNumOfInnings, pointValue) => {
+  let point = pointValue;
+  for (let i = 0; i < predictedNumOfInnings; i++) {
+    if (predictedNumOfInnings == 1) {
+      return point;
     }
     point = point + point;
   }
--- server/calculations/shutoutPoints.js
+++ server/calculations/shutoutPoints.js
@@ -1,7 +1,9 @@
 exports.oneTeamShutout = (sport) => {
   //award 10 points
+  return 10;
 };
 
 exports.twoTeamShutout = (sport) => {
   //award 30 points
+  return 30;
 };
