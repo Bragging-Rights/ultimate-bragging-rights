d4b24a81d34d1ef45727df6e933a3bff24a80bc2
Muhammad Umar <muhammadumar03124701209@gmail.com>
2024-02-26 14:47:18 +0500

    Update routes.js file

---
+++ frontend/src/pages/FbChallanges.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import MainNavBar from '../components/MainNavBar'
+
+const FbChallanges = () => {
+  return (
+    <div className=' w-full'>
+    <MainNavBar/>
+  </div>
+  )
+}
+
+export default FbChallanges
