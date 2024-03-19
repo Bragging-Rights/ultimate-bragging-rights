d15cb6e2e8ddf7db2e066d87c1574e898cd4021e
Muhammad Umar <muhammadumar03124701209@gmail.com>
2024-03-19 05:04:40 +0500

    Add styles for NightResult component

--- frontend/src/components/NightResult/NightResult.js
+++ frontend/src/components/NightResult/NightResult.js
@@ -1,31 +1,49 @@
 import React from "react";
-import "./NightResult.css"
+import "./NightResult.css";
+const { data, headers, headerOption } = require("./dump");
 
-const headerOption = [
-  "Visitor",
-  "Home",
-  "Final",
-  "Prediction",
-  "Time",
-  "CO",
-  "PROV",
-];
 const NightResult = () => {
   const tableCellStyle = {
-    fontSize: '0.75rem',
-    fontWeight: '500',
-    textAlign: 'center',
-    padding: '0.5rem',
-    border: '1px   #ddd', // Border color
+    fontSize: "0.75rem",
+    fontWeight: "500",
+    textAlign: "center",
+    padding: "0.5rem",
+    border: "1px   #ddd", // Border color
   };
 
   const headerCellStyle = {
     ...tableCellStyle,
-    backgroundColor: '#181818',
-    color: 'white',
-    height: '2.5rem',
+    backgroundColor: "#181818",
+    color: "white",
+    height: "2.5rem",
   };
 
+  const SvgIcon = () => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={39}
+      height={39}
+      viewBox="0 0 39 39"
+      fill="none"
+    >
+      <circle
+        cx="19.1313"
+        cy="19.1875"
+        r="14.2617"
+        stroke="black"
+        strokeWidth={4}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M23.8853 16.0181L17.5467 22.3566L14.3774 19.1873"
+        stroke="black"
+        strokeWidth={4}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
 
   return (
     <table className=" mt-5 w-full table-auto border-separate ">
@@ -34,506 +52,62 @@ const NightResult = () => {
           {headerOption?.map((item, ind) => {
             return (
               <th
-              key={ind}
-              style={{
-                background:
-                  "linear-gradient(180deg, #BE8200 0%, #FEF098 47.4%, #EFD261 100%)",
-                backgroundClip: "text",
-                WebkitBackgroundClip: "text",
-                WebkitTextFillColor: "transparent",
-                fontSize: "0.75rem",
-                fontWeight: "500",
-                color: "#FEF098",
-              }}
-            >
-              {/* {item} */}
-            </th>
+                key={ind}
+                style={{
+                  background:
+                    "linear-gradient(180deg, #BE8200 0%, #FEF098 47.4%, #EFD261 100%)",
+                  backgroundClip: "text",
+                  WebkitBackgroundClip: "text",
+                  WebkitTextFillColor: "transparent",
+                  fontSize: "0.75rem",
+                  fontWeight: "500",
+                  color: "#FEF098",
+                }}
+              ></th>
             );
           })}
-          <th
-             style={{
-                textTransform: "uppercase",
-                fontSize: "0.75rem",
-                fontWeight: "500",
-                color: "#FEF098",
-              }}
-          >
-            MINNESOTA <br /> vs <br />
-            Tampa bay
-          </th>
-          <th
-             style={{
-                textTransform: "uppercase",
-                fontSize: "0.75rem",
-                fontWeight: "500",
-                color: "#FEF098",
-              }}
-          >
-            MINNESOTA <br /> vs <br />
-            Tampa bay
-          </th>
-          <th
-             style={{
-                textTransform: "uppercase",
-                fontSize: "0.75rem",
-                fontWeight: "500",
-                color: "#FEF098",
-              }}
-          >
-            MINNESOTA <br /> vs <br />
-            Tampa bay
-          </th>
-          <th
-             style={{
-                textTransform: "uppercase",
-                fontSize: "0.75rem",
-                fontWeight: "500",
-                color: "#FEF098",
-              }}
-          >
-            MINNESOTA <br /> vs <br />
-            Tampa bay
-          </th>
-          <th
-             style={{
-                textTransform: "uppercase",
-                fontSize: "0.75rem",
-                fontWeight: "500",
-                color: "#FEF098",
-              }}
-          >
-            MINNESOTA <br /> vs <br />
-            Tampa bay
-          </th>
-          <th
-             style={{
-                textTransform: "uppercase",
-                fontSize: "0.75rem",
-                fontWeight: "500",
-                color: "#FEF098",
-              }}
-          >
-            MINNESOTA <br /> vs <br />
-            Tampa bay
-          </th>
-          <th
-             style={{
-                textTransform: "uppercase",
-                fontSize: "0.75rem",
-                fontWeight: "500",
-                color: "#FEF098",
-              }}
-          >
-            MINNESOTA <br /> vs <br />
-            Tampa bay
-          </th>
         </tr>
       </thead>
       <tbody>
-      <tr style={headerCellStyle}>
-      {/* <td style={tableCellStyle}>CO</td> */}
-      <td style={tableCellStyle}>
-  City <br /> Prov/State
-</td>  
-    <td style={tableCellStyle}>PLAYER</td>
-      <td style={tableCellStyle}>Rank</td>
-      <td style={tableCellStyle}>TP</td>
-      <td style={tableCellStyle}>BR</td>
-      <td style={tableCellStyle}>W%</td>
-      <td style={tableCellStyle}>W-#</td>
-      <td style={tableCellStyle}>L-#</td>
-      <td style={tableCellStyle}>APG</td>
-      <td style={tableCellStyle}>CS</td>
-      <td style={tableCellStyle}>Game 1 <br/> Visitor/Home(pts)</td>
-      <td style={tableCellStyle}>Game 2 <br/> Visitor/Home(pts)</td>
-      <td style={tableCellStyle}>Game 3 <br/> Visitor/Home(pts)</td>
-      <td style={tableCellStyle}>Game 4 <br/> Visitor/Home(pts)</td>
-
-    
-    </tr>
-    <tr>
-      <td
-        style={{
-          fontSize: '0.75rem',
-          fontWeight: 'bold',
-          textAlign: 'center',
-          color: '#FFB800',
-        }}
-      >
-        CA
-      </td>
-      <td
-        style={{
-          fontSize: '0.75rem',
-          fontWeight: 'bold',
-          textAlign: 'center',
-          color: 'white',
-        }}
-      >
-        St. John’s NL
-      </td>
-      <td
-        style={{
-          fontSize: '0.75rem',
-          fontWeight: 'bold',
-          textAlign: 'center',
-          color: '#FFB800',
-        }}
-      >
-        Topdog
-      </td>
-      <td
-        style={{
-          fontSize: '0.75rem',
-          fontWeight: 'bold',
-          textAlign: 'center',
-          color: 'white',
-        }}
-      >
-        2
-      </td>
-      <td
-        style={{
-          fontSize: '0.75rem',
-          fontWeight: 'bold',
-          textAlign: 'center',
-          color: 'white',
-        }}
-      >
-        24 Pts
-      </td>
-      <td
-        style={{
-          background:
-            'linear-gradient(180deg, #BE8200 0%, #FEF098 47.4%, #EFD261 100%)',
-        }}
-      >
-
-          <span style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width={39}
-            height={39}
-            viewBox="0 0 39 39"
-            fill="none"
-          >
-            <circle
-              cx="19.1313"
-              cy="19.1875"
-              r="14.2617"
-              stroke="black"
-              strokeWidth={4}
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-            <path
-              d="M23.8853 16.0181L17.5467 22.3566L14.3774 19.1873"
-              stroke="black"
-              strokeWidth={4}
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
-        </span>
-      </td>
-      <td
-        style={{
-          backgroundColor: '#62C555',
-          fontSize: '1rem',
-          fontWeight: 'bold',
-          width: '2.5rem',
-          textAlign: 'center',
-          textShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
-          color: 'black',
-        }}
-      >
-        36
-      </td>
-      <td
-        style={{
-          backgroundColor: '#62C555',
-          fontSize: '1rem',
-          fontWeight: 'bold',
-          width: '2.5rem',
-          textAlign: 'center',
-          textShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
-          color: '#F8FA13',
-        }}
-      >
-        36
-      </td>
-      <td
-        style={{
-          backgroundColor: '#E61C1C',
-          fontSize: '1rem',
-          fontWeight: 'bold',
-          width: '2.5rem',
-          textAlign: 'center',
-          textShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
-          color: '#F8FA13',
-        }}
-      >
-        2
-      </td>
-      <td
-  style={{
-    background:
-      'linear-gradient(180deg, #BE8200 0%, #FEF098 47.4%, #EFD261 100%)',
-    fontSize: '1rem',
-    fontWeight: 'bold',
-    width: '2.5rem',
-    textAlign: 'center',
-    textShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
-    color: 'black',
-    filter: 'blur(2px)',
-  }}
->
-  23
-</td>
-      <td
-  style={{
-    backgroundColor: '#62C555',
-    fontSize: '1rem',
-    fontWeight: 'bold',
-    width: '2.5rem',
-    textAlign: 'center',
-    textShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
-    color: '#F8FA13',
-  }}
->
-  0
-</td>
-<td
-  style={{
-    backgroundColor: '#62C555',
-    fontSize: '1rem',
-    fontWeight: 'bold',
-    width: '2.5rem',
-    textAlign: 'center',
-    textShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
-    color: '#F8FA13',
-  }}
->
-  2
-</td>
-<td
-  style={{
-    backgroundColor: '#62C555',
-    fontSize: '1rem',
-    fontWeight: 'bold',
-    width: '2.5rem',
-    textAlign: 'center',
-    textShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
-    color: '#F8FA13',
-  }}
->
-  2
-</td>
-<td
-  style={{
-    backgroundColor: '#62C555',
-    fontSize: '1rem',
-    fontWeight: 'bold',
-    width: '2.5rem',
-    textAlign: 'center',
-    textShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
-    color: '#F8FA13',
-  }}
->
-  2
-</td>
+        <tr style={headerCellStyle}>
+          {headers.map((header, index) => (
+            <td key={index} style={tableCellStyle}>
+              {header.split("\n").map((text, i) => (
+                <React.Fragment key={i}>
+                  {text}
+                  <br />
+                </React.Fragment>
+              ))}
+            </td>
+          ))}
         </tr>
-        <tr style={{ backgroundColor: '#1B1C21' }}>
-      <td
-        style={{
-          fontSize: '0.75rem',
-          fontWeight: 'bold',
-          textAlign: 'center',
-          color: '#FFB800',
-        }}
-      >
-        CA
-      </td>
-      <td
-        style={{
-          fontSize: '0.75rem',
-          fontWeight: 'bold',
-          textAlign: 'center',
-          color: 'white',
-        }}
-      >
-        St. John’s NL
-      </td>
-      <td
-        style={{
-          fontSize: '0.75rem',
-          fontWeight: 'bold',
-          textAlign: 'center',
-          color: '#FFB800',
-        }}
-      >
-        Topdog
-      </td>
-      <td
-        style={{
-          fontSize: '0.75rem',
-          fontWeight: 'bold',
-          textAlign: 'center',
-          color: 'white',
-        }}
-      >
-        2
-      </td>
-      <td
-        style={{
-          fontSize: '0.75rem',
-          fontWeight: 'bold',
-          textAlign: 'center',
-          color: 'white',
-        }}
-      >
-        24 Pts
-      </td>
-      <td
-        style={{
-          background:
-            'linear-gradient(180deg, #BE8200 0%, #FEF098 47.4%, #EFD261 100%)',
-        }}
-      >
-        <span style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width={39}
-            height={39}
-            viewBox="0 0 39 39"
-            fill="none"
+        {data.map((item, index) => (
+          <tr
+            key={index}
+            style={{
+              backgroundColor: item.backgroundColor,
+            }}
           >
-            <circle
-              cx="19.1313"
-              cy="19.1875"
-              r="14.2617"
-              stroke="black"
-              strokeWidth={4}
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-            <path
-              d="M23.8853 16.0181L17.5467 22.3566L14.3774 19.1873"
-              stroke="black"
-              strokeWidth={4}
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
-        </span>
-      </td>
-      <td
-  style={{
-    backgroundColor: '#62C555',
-    fontSize: '1rem',
-    fontWeight: 'bold',
-    width: '2.5rem',
-    textAlign: 'center',
-    textShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
-    color: 'black',
-  }}
->
-  36
-</td>
-<td
-  style={{
-    backgroundColor: '#62C555',
-    fontSize: '1rem',
-    fontWeight: 'bold',
-    width: '2.5rem',
-    textAlign: 'center',
-    textShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
-    color: '#F8FA13',
-  }}
->
-  36
-</td>
-<td
-  style={{
-    backgroundColor: '#E61C1C',
-    fontSize: '1rem',
-    fontWeight: 'bold',
-    width: '2.5rem',
-    textAlign: 'center',
-    textShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
-    color: '#F8FA13',
-  }}
->
-  2
-</td>
-<td
-  style={{
-    background:
-      'linear-gradient(180deg, #BE8200 0%, #FEF098 47.4%, #EFD261 100%)',
-    fontSize: '1rem',
-    fontWeight: 'bold',
-    width: '2.5rem',
-    textAlign: 'center',
-    textShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
-    color: 'black',
-    filter: 'blur(2px)',
-  }}
->
-  23
-</td>
-<td
-  style={{
-    backgroundColor: '#62C555',
-    fontSize: '1rem',
-    fontWeight: 'bold',
-    width: '2.5rem',
-    textAlign: 'center',
-    textShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
-    color: '#F8FA13',
-  }}
->
-  0
-</td>
-<td
-  style={{
-    backgroundColor: '#62C555',
-    fontSize: '1rem',
-    fontWeight: 'bold',
-    width: '2.5rem',
-    textAlign: 'center',
-    textShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
-    color: '#F8FA13',
-  }}
->
-  2
-</td>
-<td
-  style={{
-    backgroundColor: '#62C555',
-    fontSize: '1rem',
-    fontWeight: 'bold',
-    width: '2.5rem',
-    textAlign: 'center',
-    textShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
-    color: '#F8FA13',
-  }}
->
-  2
-</td>
-<td
-  style={{
-    backgroundColor: '#62C555',
-    fontSize: '1rem',
-    fontWeight: 'bold',
-    width: '2.5rem',
-    textAlign: 'center',
-    textShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
-    color: '#F8FA13',
-  }}
->
-  2
-</td>
-        </tr>
+            <td className="nr-state-rank">{item.city}</td>
+            <td className="nr-player-tp-rank">{item.player}</td>
+            <td className="nr-state-rank">{item.rank}</td>
+            <td className="nr-player-tp-rank">{item.tp}</td>
+            <td className="nr-player-tp-rank">{item.br}</td>
+            <td className="nr-bg">
+              <span className="nr-svg-span">
+                <SvgIcon />
+              </span>
+            </td>
+            <td className="nr-details-2">{item.w}</td>
+            <td className="nr-details">{item.l}</td>
+            <td className="nr-details-3">{item.apg}</td>
+            <td className="nr-details-1">{item.cs}</td>
+            <td className="nr-details">{item.game1}</td>
+            <td className="nr-details">{item.game2}</td>
+            <td className="nr-details">{item.game3}</td>
+            <td className="nr-details">{item.game4}</td>
+          </tr>
+        ))}
       </tbody>
     </table>
   );
