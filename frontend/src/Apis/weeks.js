a57417996095b918e7e7ea8b9f3b9bb914c52d64
Muhammad Umar <muhammadumar03124701209@gmail.com>
2024-03-31 22:55:51 +0500

    Add week routes and related API functions

---
+++ frontend/src/Apis/weeks.js
@@ -0,0 +1,31 @@
+import api from "./api";
+
+// Create a new week
+export const createWeek = async (week) => {
+  const response = await api.post("api/weeks", JSON.stringify(week));
+  return response;
+};
+
+// Get all weeks
+export const getWeeks = async () => {
+  const response = await api.get("api/weeks");
+  return response;
+};
+
+// Get a week by id
+export const getWeek = async (id) => {
+  const response = await api.get(`api/weeks/${id}`);
+  return response;
+};
+
+// Update a week by id
+export const updateWeek = async (id, week) => {
+  const response = await api.put(`api/weeks/${id}`, JSON.stringify(week));
+  return response;
+};
+
+// Delete a week by id
+export const deleteWeek = async (id) => {
+  const response = await api.delete(`api/weeks/${id}`);
+  return response;
+};
