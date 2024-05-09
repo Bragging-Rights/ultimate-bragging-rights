aea8fafe13a0a1bbaa1da1da1b25ff8a3011cdc6
Usman <usmansaeed6479@gmail.com>
2024-05-09 21:30:33 +0500

    updated ML

--- frontend/src/Layout/MainLayout.js
+++ frontend/src/Layout/MainLayout.js
@@ -6,6 +6,7 @@ import SearchBar from "../components/SearchBar/SearchBar";
 import resultHero from "../assets/bgpic.png";
 import HeroSection from "../components/HeroSection";
 import MainNavBar from "../components/MainNavBar";
+import { Box, Typography } from "@mui/material";
 
 const MainLayout = ({ children }) => {
   const location = useLocation();
@@ -22,11 +23,26 @@ const MainLayout = ({ children }) => {
   return (
     <>
       <Navbar />
-      <br/>
+      <br />
       <HeroSection imgUrl={resultHero} alt="img" />
       <br />
       <MainNavBar />
       <br />
+      <Box sx={{ border: "2px solid red", height: "50px" }}>
+        <Typography
+          sx={{
+            fontSize: "2rem",
+            color: "white",
+            whiteSpace: "nowrap",
+            overflow: "hidden",
+          }}
+        >
+          <marquee className="marq" direction="left" loop="">
+            Results are updated hourly until we find a better solution
+          </marquee>
+        </Typography>
+      </Box>
+
       {showsearchBar && <SearchBar />}
 
       <div className="flex justify-content">
