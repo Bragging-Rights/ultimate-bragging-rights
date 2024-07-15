c3f93007fc128452549379486066174e5fa784ac
jraza3363 <jraza3363@gmail.com>
2024-07-15 22:31:38 +0500

    scrollbar to stats page table

--- frontend/src/pages/Leaderboard/Hockey.js
+++ frontend/src/pages/Leaderboard/Hockey.js
@@ -126,76 +126,71 @@ export default function Statspage() {
   };
 
   return (
-    <div>
-      <div style={{ backgroundColor: 'rgb(179, 179, 0)', display: 'flex', justifyContent: 'flex-start', alignItems: 'center', padding: '10px' }}>
+    <div >
+      <div style={{ backgroundColor: 'rgb(179, 179, 0)' , display: 'flex', justifyContent: 'flex-start', alignItems: 'center', padding: '10px' }}>
         <span>{formattedDate}</span>
         <span style={{ textAlign: 'center', color: 'black', flexGrow: 1 }}>Game Breakdowns</span>
       </div>
-      <button  style={{ backgroundColor: '#1B1C21',color:"white",padding:"1rem",border:"none" }}>League</button>
-      <button  style={{ backgroundColor: '#1B1C21',color:"white",padding:"1rem",border:"none" }}>Home</button>
-      <button style={{backgroundColor:"#1B1C21",color:"white",padding:"1rem",border:"none" }}>Away</button>
-      <div style={{ color: 'white' }}>
-        <div className="grid-item">
-          <table className="custom-table">
-            <thead>
-              <tr>
-                <th>Team</th>
-                <th onClick={() => toggleSortOrder('home')} style={{ cursor: 'pointer' }}>Gp</th>
-                <th onClick={() => toggleSortOrder('final')} style={{ cursor: 'pointer' }}>W</th>
-                <th onClick={() => toggleSortOrder('prediction')} style={{ cursor: 'pointer' }}>L</th>
-                <th onClick={() => toggleSortOrder('time')} style={{ cursor: 'pointer' }}>UBR</th>
-                <th onClick={() => toggleSortOrder('co')} style={{ cursor: 'pointer' }}>APG</th>
-                <th onClick={() => toggleSortOrder('prov')} style={{ cursor: 'pointer' }}>APW</th>
-                <th onClick={() => toggleSortOrder('state')} style={{ cursor: 'pointer' }}>WS</th>
-                <th onClick={() => toggleSortOrder('city')} style={{ cursor: 'pointer' }}>LS</th>
-                <th onClick={() => toggleSortOrder('player')} style={{ cursor: 'pointer' }}>PTS/F</th>
-                <th onClick={() => toggleSortOrder('R')} style={{ cursor: 'pointer' }}>RANK</th>
-                <th onClick={() => toggleSortOrder('TP')} style={{ cursor: 'pointer' }}>PTS/A</th>
-                <th onClick={() => toggleSortOrder('Br')} style={{ cursor: 'pointer' }}>F</th>
-                <th onClick={() => toggleSortOrder('Fu')} style={{ cursor: 'pointer' }}>U/D</th>
-                <th onClick={() => toggleSortOrder('odds')} style={{ cursor: 'pointer' }}>SPRD</th>
-                <th onClick={() => toggleSortOrder('accuracy')} style={{ cursor: 'pointer' }}>O/U</th>
-                <th onClick={() => toggleSortOrder('shutout')} style={{ cursor: 'pointer' }}>1S</th> 
-              
-                <th onClick={() => toggleSortOrder('E1')} style={{ cursor: 'pointer' }}>REG</th>
-                <th onClick={() => toggleSortOrder('SO')} style={{ cursor: 'pointer' }}>OT</th>
-                <th onClick={() => toggleSortOrder('SOI')} style={{ cursor: 'pointer' }}>S/O</th>
-                <th onClick={() => toggleSortOrder('SO1')} style={{ cursor: 'pointer' }}>1SO</th>
-              
+      <div style={{ display: 'flex', justifyContent: '', marginTop: '10px' }}>
+        <button style={{ backgroundColor: 'black', color: 'white', padding: '1rem', border: 'none', marginRight: '10px' }}>League</button>
+        <button style={{ backgroundColor: 'black', color: 'white', padding: '1rem', border: 'none', marginRight: '10px' }}>Home</button>
+        <button style={{ backgroundColor: 'black', color: 'white', padding: '1rem', border: 'none' }}>Away</button>
+      </div>
+      <div style={{ color: 'white', marginTop: '10px', overflowX: 'auto' }}>
+        <table className="custom-table">
+          <thead>
+            <tr>
+              <th>Team</th>
+              <th onClick={() => toggleSortOrder('home')} style={{ cursor: 'pointer' }}>Gp</th>
+              <th onClick={() => toggleSortOrder('final')} style={{ cursor: 'pointer' }}>W</th>
+              <th onClick={() => toggleSortOrder('prediction')} style={{ cursor: 'pointer' }}>L</th>
+              <th onClick={() => toggleSortOrder('time')} style={{ cursor: 'pointer' }}>UBR</th>
+              <th onClick={() => toggleSortOrder('co')} style={{ cursor: 'pointer' }}>APG</th>
+              <th onClick={() => toggleSortOrder('prov')} style={{ cursor: 'pointer' }}>APW</th>
+              <th onClick={() => toggleSortOrder('state')} style={{ cursor: 'pointer' }}>WS</th>
+              <th onClick={() => toggleSortOrder('city')} style={{ cursor: 'pointer' }}>LS</th>
+              <th onClick={() => toggleSortOrder('player')} style={{ cursor: 'pointer' }}>PTS/F</th>
+              <th onClick={() => toggleSortOrder('R')} style={{ cursor: 'pointer' }}>RANK</th>
+              <th onClick={() => toggleSortOrder('TP')} style={{ cursor: 'pointer' }}>PTS/A</th>
+              <th onClick={() => toggleSortOrder('Br')} style={{ cursor: 'pointer' }}>F</th>
+              <th onClick={() => toggleSortOrder('Fu')} style={{ cursor: 'pointer' }}>U/D</th>
+              <th onClick={() => toggleSortOrder('odds')} style={{ cursor: 'pointer' }}>SPRD</th>
+              <th onClick={() => toggleSortOrder('accuracy')} style={{ cursor: 'pointer' }}>O/U</th>
+              <th onClick={() => toggleSortOrder('shutout')} style={{ cursor: 'pointer' }}>1S</th>
+              <th onClick={() => toggleSortOrder('E1')} style={{ cursor: 'pointer' }}>REG</th>
+              <th onClick={() => toggleSortOrder('SO')} style={{ cursor: 'pointer' }}>OT</th>
+              <th onClick={() => toggleSortOrder('SOI')} style={{ cursor: 'pointer' }}>S/O</th>
+              <th onClick={() => toggleSortOrder('SO1')} style={{ cursor: 'pointer' }}>1SO</th>
+            </tr>
+          </thead>
+          <tbody>
+            {sortedData.map((data, index) => (
+              <tr key={index}>
+                <td>{data.visitor}</td>
+                <td>{data.home}</td>
+                <td>{data.final}</td>
+                <td>{data.prediction}</td>
+                <td>{data.time}</td>
+                <td>{data.co}</td>
+                <td>{data.prov}</td>
+                <td>{data.state}</td>
+                <td>{data.city}</td>
+                <td>{data.player}</td>
+                <td>{data.R}</td>
+                <td>{data.TP}</td>
+                <td>{data.Br}</td>
+                <td>{data.Fu}</td>
+                <td>{data.odds}</td>
+                <td>{data.accuracy}</td>
+                <td>{data.shutout}</td>
+                <td>{data.E1}</td>
+                <td>{data.SO}</td>
+                <td>{data.SOI}</td>
+                <td>{data.SO1}</td>
               </tr>
-            </thead>
-            <tbody>
-              {sortedData.map((data, index) => (
-                <tr key={index}>
-                  <td>{data.visitor}</td>
-                  <td>{data.home}</td>
-                  <td>{data.final}</td>
-                  <td>{data.prediction}</td>
-                  <td>{data.time}</td>
-                  <td>{data.co}</td>
-                  <td>{data.prov}</td>
-                  <td>{data.state}</td>
-                  <td>{data.city}</td>
-                  <td>{data.player}</td>
-                  <td>{data.R}</td>
-                  <td>{data.TP}</td>
-                  <td>{data.Br}</td>
-                  <td>{data.Fu}</td>
-                  <td>{data.odds}</td>
-                  <td>{data.accuracy}</td>
-                  <td>{data.shutout}</td>
-                 
-                  <td>{data.E1}</td>
-                  <td>{data.SO}</td>
-                  <td>{data.SOI}</td>
-                  <td>{data.SO1}</td>
-                
-                  
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
   );
