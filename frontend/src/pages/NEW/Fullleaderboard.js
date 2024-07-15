3dd38ac3604c0f8299072752ce37544b6309e66c
jraza3363 <jraza3363@gmail.com>
2024-07-15 22:41:22 +0500

    scroll bar to testleader page

--- frontend/src/pages/NEW/Fullleaderboard.js
+++ frontend/src/pages/NEW/Fullleaderboard.js
@@ -1,5 +1,7 @@
 
 import React, { useState, useEffect } from 'react';
+import './TableStyles.css';
+
 
 export default function Fullleaderboard() {
   const [currentDate, setCurrentDate] = useState(new Date());
