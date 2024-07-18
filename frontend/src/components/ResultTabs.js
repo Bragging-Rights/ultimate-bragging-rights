4d89ecedbc445bab0de709b95708507b32f5d85b
jraza3363 <jraza3363@gmail.com>
2024-07-18 18:39:19 +0500

    ResultsTab updated

--- frontend/src/components/ResultTabs.js
+++ frontend/src/components/ResultTabs.js
@@ -43,13 +43,13 @@ const ResultTabs = ({ changeTab, CurrentTab }) => {
           sx={{
             display: "flex",
             justifyContent: "space-around",
-            width: isMobileView ? "50vh" : "57%",
-            height: isMobileView ? "10vh" : "10vh",
+            width: isMobileView ? "30vh" : "57%",
+            height: isMobileView ? "8vh" : "10vh",
             backgroundColor: "black",
             borderRadius: "5vh",
             position: "relative",
             overflow: "hidden",
-            marginLeft: "21%",
+            marginLeft: isMobileView ? "15%" : "21%",
           }}
         >
           {tabs.map((tab, index) => (
@@ -98,43 +98,6 @@ const ResultTabs = ({ changeTab, CurrentTab }) => {
             </ListItem>
           ))}
         </List>
-        {/* <List
-        sx={{
-          display: "flex",
-          height: "12vh",
-          alignItems: "center",
-          borderBottom: "1px solid #393939",
-          marginBottom: "2vh",
-        }}
-      >
-        {tabs[selectedTab].options.map((option, index) => (
-          <ListItem
-            key={index + tabs.length}
-            onClick={() => changeTab(index + tabs.length)}
-            sx={{
-              cursor: "pointer",
-              flex: "0 0 13vh",
-              textAlign: "center",
-              padding: "8px",
-              color: "white", // Set text color to white
-              backgroundColor:
-                CurrentTab === index + tabs.length ? "#FF0000" : "transparent",
-              border:
-                CurrentTab === index + tabs.length
-                  ? "1px solid #BE8200"
-                  : "none",
-              borderRadius: "8px",
-              boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
-              transition: "background-color 0.3s ease",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <Typography variant="body1">{option}</Typography>
-          </ListItem>
-        ))}
-      </List> */}
       </Box>
     </Container>
   );
